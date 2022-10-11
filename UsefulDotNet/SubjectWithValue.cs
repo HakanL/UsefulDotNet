acd65f103b799dc467e5dd52c4a7859ee55e1388
Hakan Lindestaf <hakan@lindestaf.com>
2022-10-11 14:48:28 -0500

    Added option for distinct

--- UsefulDotNet/SubjectWithValue.cs
+++ UsefulDotNet/SubjectWithValue.cs
@@ -8,10 +8,12 @@ namespace System.Reactive.Subjects
     public class SubjectWithValue<T> : ISubject<T>, ISubjectWithValue<T>
     {
         private readonly Subject<T> subject;
+        private readonly bool distinct;
 
-        public SubjectWithValue(T initialValue)
+        public SubjectWithValue(T initialValue, bool distinct = false)
         {
             Value = initialValue;
+            this.distinct = distinct;
 
             this.subject = new Subject<T>();
         }
@@ -30,8 +32,11 @@ namespace System.Reactive.Subjects
 
         public void OnNext(T value)
         {
-            Value = value;
-            this.subject.OnNext(value);
+            if (!this.distinct || !Value.Equals(value))
+            {
+                Value = value;
+                this.subject.OnNext(value);
+            }
         }
 
         public IDisposable Subscribe(IObserver<T> observer)
